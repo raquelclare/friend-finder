b1e9aeaf1d93f9ad1e2ddfc603f1d9a740b53543
Raquel Rodriguez <raquelclare@Raquels-MacBook-Air.local>
2017-07-13 15:31:19 -0500

    setting up server.js file

---
+++ server.js
@@ -0,0 +1,49 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Questionaire (DATA)
+var friends = [];
+
+// ROUTES
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "survey.html"));
+});
+
+// Route getting info for all friends - provides JSON
+app.get("/api/friends", function(req, res) {
+  res.json(friends);
+});
+
+// Create new survey/friend for app - takes in JSON input
+app.post("/api/new", function(req, res) {
+  var newFriend = req.body;
+  newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newFriend;
+
+  friends.push(newFriend);
+
+  res.json(newFriend);
+});
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
